e3529096f11df8ce7505e4cd5270a9a0ff851981
zxx <740295395@qq.com>
2023-12-19 18:39:48 +0800

    fix

--- internal/types/types.go
+++ internal/types/types.go
@@ -6,10 +6,31 @@ type InvokeParams struct {
 }
 
 type DingMessageSendRequest struct {
-	Text           *text  `json:"text"`
-	ConversationId string `json:"conversationId"`
+	Text                      *text      `json:"text"`
+	ConversationId            string     `json:"conversationId"`
+	AtUsers                   []*atUsers `json:"atUsers"`
+	ChatbotCorpId             string     `json:"chatbotCorpId"`
+	ChatbotUserId             string     `json:"chatbotUserId"`
+	MsgId                     string     `json:"msgId"`
+	SenderNick                string     `json:"senderNick"`
+	IsAdmin                   bool       `json:"isAdmin"`
+	SenderStaffId             string     `json:"senderStaffId"`
+	SessionWebhookExpiredTime int64      `json:"sessionWebhookExpiredTime"`
+	CreateAt                  int64      `json:"createAt"`
+	SenderCorpId              string     `json:"senderCorpId"`
+	ConversationType          string     `json:"conversationType"`
+	SenderId                  string     `json:"senderId"`
+	ConversationTitle         string     `json:"conversationTitle"`
+	IsInAtList                bool       `json:"isInAtList"`
+	SessionWebhook            string     `json:"sessionWebhook"`
+	RobotCode                 string     `json:"robotCode"`
+	Msgtype                   string     `json:"msgtype"`
 }
 
 type text struct {
 	Content string `json:"content"`
+}
+
+type atUsers struct {
+	DingtalkId string `json:"dingtalkId"`
 }
