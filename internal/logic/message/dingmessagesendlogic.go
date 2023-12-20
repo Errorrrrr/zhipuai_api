210892acc974b9ef3fdf2535a58de482faf841fc
zxx <740295395@qq.com>
2023-12-20 15:05:00 +0800

    fix

--- internal/logic/message/dingmessagesendlogic.go
+++ internal/logic/message/dingmessagesendlogic.go
@@ -35,7 +35,7 @@ func (l *DingMessageSendLogic) DingMessageSend(req *types.DingMessageSendRequest
 	prompt := []*dto.Prompt{
 		{
 			RoleName: enum.User,
-			Content:  req.Text.Content + "。使用Markdown格式返回",
+			Content:  req.Text.Content + "。使用Markdown格式进行回复",
 		},
 	}
 	body := &dto.AiRequest{
