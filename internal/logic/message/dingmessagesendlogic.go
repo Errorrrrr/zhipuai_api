2a8ce926aa57bbfed7f45e1c9aa08b072788cd1f
zxx <740295395@qq.com>
2023-12-20 13:59:54 +0800

    fix

--- internal/logic/message/dingmessagesendlogic.go
+++ internal/logic/message/dingmessagesendlogic.go
@@ -2,7 +2,10 @@ package message
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"zhipuai_api/internal/common/dto"
+	"zhipuai_api/internal/common/enum"
 	logic "zhipuai_api/internal/logic/common"
 	"zhipuai_api/internal/svc"
 	"zhipuai_api/internal/types"
@@ -28,6 +31,33 @@ func (l *DingMessageSendLogic) DingMessageSend(req *types.DingMessageSendRequest
 	if req.Text.Content == "" {
 		return errors.New("content is empty")
 	}
+	prompt := []*dto.Prompt{
+		{
+			RoleName: enum.User,
+			Content:  req.Text.Content,
+		},
+	}
+	body := &dto.AiRequest{
+		Model:       enum.ChatglmTurbo,
+		Prompt:      prompt,
+		TopP:        0.9,
+		Temperature: 0.9,
+	}
+	ans, err := logic.Send(body, enum.ChatglmTurbo, enum.Invoke)
+	if err != nil {
+		return
+	}
+	aiResp := new(dto.AiResponse)
+	err = json.Unmarshal([]byte(ans), aiResp)
+	if err != nil {
+		return
+	}
+	if aiResp.Code != 200 || len(aiResp.Data.Choices) <= 0 {
+		return
+	}
+
+	req.Text.Content = aiResp.Data.Choices[0].Content
+
 	err = logic.DingSend(req)
 	return
 }
